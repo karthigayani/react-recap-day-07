4aca3b35746535d7dc6851006382b6472e68f8d1
karthigayani <karthi1826@gmail.com>
2023-04-02 09:43:19 +0530

    react-recap-day-07

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // ---- js starts here ----
+
+  // ---- js ends here ----
   return (
+    // ---- jsx starts here ----
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* component call */}
+      <Dashboard />
+    </div>
+    // ---- jsx endss here ----
+  );
+}
+
+// component declaration
+function Dashboard(){
+  return (
+    <div>
+      <h1>Welcome to Dashlead 🎉🎊✨</h1>
+      <SummaryBox />
+    </div>
+  );
+}
+
+function SummaryBox(){
+  const data = {
+    text:"Number Of Sales",
+    number: 568,
+    percent: 0.7,
+    performance: "up",
+    time: "Last Month",
+    // type: "money",
+    type: "number",
+  }
+  return (
+    <div className="summary-box-container">
+      <p className="summary-box-text">{data.text}</p>
+      <h2 className="summary-box-number">
+        {data.type === "money" ? "$" : null}
+        {data.number}
+        </h2>
+      <div className="summary-box-time-container">
+        <p>{data.time}</p>
+        <p>{data.percent}%</p>
+      </div>
     </div>
   );
 }
---
+++ src/notes.md
@@ -0,0 +1,28 @@
+### recap notes
+
+- What is the comment to create a react App ?
+  `npx create-react-app appname`
+  `cd ..` - one folder back
+- What is the comment to run a react App ?
+  `npm start`
+- What are the points for creating(declaring) a component?
+  `1. component name should starts with capital letter`
+  `2. It should return atleast one JSX element`
+- Which element is used to start react app ?
+  `<div id="root"></div>` which is in index.html
+- when <noscript> content display ?
+  `When you OFF the Javascript in browser ` it will display.
+- Why we use className?
+  `1. At the end JSX converted into JS, So it will make conflict`
+  `2. Reserved keyword`
+- Who convert JSX into JS?
+  `Webpack + Babel`
+
+### How to change color format while inspect ?
+
+1. click on shift and place the curser on color box , color format will change.
+2. when you click on color box while inspecting, it opens a color box dialogue box, in that you have an arrow keys to change the color format of the particular color.
+
+### Adding font awesome
+
+google search -> font awesome react -> open first link -> copy the codes under the heading `Get started` enter the comments one-by-one in your terminal.
